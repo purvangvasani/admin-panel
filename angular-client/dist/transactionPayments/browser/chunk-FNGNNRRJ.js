bb0e2e7929fe2c921d21d908407905c15e18218f
Purvang Vasani <purvangvasani4@gmail.com>
2024-08-20 19:13:37 +0530

    build

---
+++ angular-client/dist/transactionPayments/browser/chunk-FNGNNRRJ.js
@@ -0,0 +1,1 @@
+import"./chunk-ENQRHBTS.js";var o=[{path:"",loadComponent:()=>import("./chunk-XOMJ3DVQ.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
---
+++ angular-client/dist/transactionPayments/browser/chunk-SGTCB3SC.js
@@ -0,0 +1,1 @@
+import"./chunk-ENQRHBTS.js";var t=[{path:"",loadComponent:()=>import("./chunk-TNXSG43J.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
